f7435c9d92a191de2cb67d0f7a8d6d6e70bd1eb7
Savva Chernyshev <chesavva@gmail.com>
2024-05-25 22:11:00 +0300

    leetcode

---
+++ leetcode/67. Add Binary.py
@@ -0,0 +1,15 @@
+aL, bL = -len(a), -len(b)
+i, carry, res = -1, 0, ""
+
+while i >= aL or i >= bL:
+    aBit = int(a[i]) if i >= aL else 0
+    bBit = int(b[i]) if i >= bL else 0
+
+    sum = aBit + bBit + carry
+    res = str(sum % 2) + res
+    carry = sum // 2
+
+    i -= 1
+
+print("1" + res if carry else res)
+# todo Переделать
