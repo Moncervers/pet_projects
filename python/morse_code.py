539863e8526e803a69b404a5c3647bc0c4efba45
Savva Chernyshev <chesavva@gmail.com>
2024-05-29 14:39:43 +0300

    Made morse code encryption/decryption

--- python/morse_code.py
+++ python/morse_code.py
@@ -1,4 +1,4 @@
-morse_dict = {
+morse_encode_dict = {
     'A': '.-', 'B': '-...', 'C': '-.-.',
     'D': '-..', 'E': '.', 'F': '..-.',
     'G': '--.', 'H': '....', 'I': '..',
@@ -13,13 +13,32 @@ morse_dict = {
     '6': '-....', '7': '--...', '8': '---..',
     '9': '----.'
 }
+morse_decode_dict = {".-": "A", "-...": "B", "-.-.": "C",
+                     "-..": "D", ".": "E", "..-.": "F",
+                     "--.": "G", "....": "H", "..": "I",
+                     ".---": "J", "-.-": "K", ".-..": "L",
+                     "--": "M", "-.": "N", "---": "O",
+                     ".--.": "P", "--.-": "Q", ".-.": "R",
+                     "...": "S", "-": "T", "..-": "U",
+                     "...-": "V", ".--": "W", "-..-": "X",
+                     "-.--": "Y", "--..": "Z",
+                     "-----": "0", ".----": "1", "..---": "2",
+                     "...--": "3", "....-": "4", ".....": "5",
+                     "-....": "6", "--...": "7", "---..": "8",
+                     "----.": "9"}
 
-text = input().upper().split()
+encrypted_message = []
+decrypted_message = []
+text = input('Enter message to encrypt: ').upper().split()
 
 for word in text:
     for letter in word:
-        if letter in morse_dict.keys():
-            print(morse_dict[letter], end=' ')
-    print()
+        if letter in morse_encode_dict.keys():
+            encrypted_message.append(morse_encode_dict[letter])
 
-# todo make decrypter
+print(encrypted_message)
+
+for letter in encrypted_message:
+    decrypted_message.append(morse_decode_dict[letter])
+
+print(decrypted_message)
